4b1d06b49928f05035994e0688c2274d24b0fd47
JunRock <wnstjr120422@naver.com>
2024-10-15 21:50:24 +0900

    chore : JIB 배포 방식수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,32 @@ allprojects {
     }
 }
 
+project(":mealmate-api") {
+    apply(plugin = "com.google.cloud.tools.jib")
+
+    tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+        mainClass.set("io.junseok.mealmateapi.MealmateApiApplication")
+    }
+
+    jib {
+        from {
+            image = "openjdk:17"
+        }
+        to {
+            image = "${System.getenv("DOCKER_USERNAME")}/mealmate-0.0.1-api-snapshot"
+            auth {
+                username = System.getenv("DOCKER_USERNAME") ?: ""
+                password = System.getenv("DOCKER_TOKEN") ?: ""
+            }
+        }
+        container {
+            ports = listOf("8080")
+            mainClass = "io.junseok.mealmateapi.MealmateApiApplication"
+        }
+    }
+}
+
+
 subprojects {
     java {
         toolchain {
--- mealmate-api/build.gradle.kts
+++ mealmate-api/build.gradle.kts
@@ -21,19 +21,6 @@ tasks {
     }
 }
 
-jib {
-    from {
-        image = "openjdk:17"
-    }
-    to {
-        image = "${System.getenv("DOCKER_USERNAME")}/mealmate-0.0.1-api-snapshot"
-        auth {
-            username = System.getenv("DOCKER_USERNAME") ?: ""
-            password = System.getenv("DOCKER_TOKEN") ?: ""
-        }
-    }
-    container {
-        ports = listOf("8080")
-        mainClass = "io.junseok.mealmateapi.MealmateApiApplication"
-    }
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    mainClass.set("io.junseok.mealmateapi.MealmateApiApplication")
 }
