897420fe567198fa3b8a67dbab0f1c34f0d1b6dc
JunRock <wnstjr120422@naver.com>
2024-11-01 19:45:41 +0900

    feat : roomId컬럼 추가

--- mealmate-api/src/main/kotlin/io/junseok/mealmateapi/presentation/noteroom/dto/response/NoteRoomResponse.kt
+++ mealmate-api/src/main/kotlin/io/junseok/mealmateapi/presentation/noteroom/dto/response/NoteRoomResponse.kt
@@ -4,12 +4,14 @@ import io.junseok.domain.noteroommember.NoteRoomMemberInfo
 import java.time.LocalDateTime
 
 data class NoteRoomResponse(
+    val roomId:Long,
     val roomName: String,
     val message: String,
     val sendDt: LocalDateTime
 ){
     companion object{
         fun NoteRoomMemberInfo.toNoteRoomResponse()=NoteRoomResponse(
+            roomId = this.noteRoomId,
             roomName = this.roomName,
             message = this.message,
             sendDt = this.sendDt
